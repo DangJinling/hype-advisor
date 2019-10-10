be6e3f05c28e4bc80b312f3d122d51a48d412a6c
dang <jinling.dang@sktlab.com>
2019-10-11 04:10:29 +0800

    BUG FIX

---
+++ client/build/precache-manifest.765e40ccd7b54ff9c672dd816b4167ca.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c8bfdaaf2a92758fa53bcc71d44982",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a9d70a9e57552f0880f1",
+    "url": "./static/css/main.72efc774.chunk.css"
+  },
+  {
+    "revision": "d3fd3b0b1627951d96fe",
+    "url": "./static/js/2.21d5399e.chunk.js"
+  },
+  {
+    "revision": "a9d70a9e57552f0880f1",
+    "url": "./static/js/main.8d1bd0f8.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "e8f0c129d3fab97d90b3d9591c90fae2",
+    "url": "./static/media/arrow.e8f0c129.svg"
+  },
+  {
+    "revision": "b4e6eb07fdf1e15e66787ebb509d1b6c",
+    "url": "./static/media/bape.b4e6eb07.gif"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "08dbf0fc7ad9c67dc297dc682bb54921",
+    "url": "./static/media/letter_logo.08dbf0fc.png"
+  },
+  {
+    "revision": "b3c01fe7c3a7d299381d21ef54c14357",
+    "url": "./static/media/new_logo_no_border.b3c01fe7.png"
+  },
+  {
+    "revision": "08dc4f5d040cfd31d6606872ee3043d1",
+    "url": "./static/media/overlay.08dc4f5d.png"
+  },
+  {
+    "revision": "3409f7f77b3493a31cbbf50ee6719a8f",
+    "url": "./static/media/service.3409f7f7.png"
+  },
+  {
+    "revision": "164df38a9cb55fd522a2fa160d1db727",
+    "url": "./static/media/travis_af1.164df38a.gif"
+  }
+]);
