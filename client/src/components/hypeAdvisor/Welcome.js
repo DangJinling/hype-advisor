5367cbfc61be620f308cab7ae246b490e338b5b0
dang <jinling.dang@sktlab.com>
2019-10-11 18:34:32 +0800

    BUG FIX

--- client/src/components/hypeAdvisor/Welcome.js
+++ client/src/components/hypeAdvisor/Welcome.js
@@ -7,21 +7,20 @@ export class Welcome extends Component {
         super(props)
         this.state = {
             user: props.user,
-            is_superuser: true,
             currentTab: "Subscribed"
         }
     }
 
     clickTab = (e) => {
-        this.setState({ currentTab: e.target.name}, ()=>{
+        this.setState({ currentTab: e.target.name }, () => {
 
         });
-       
+
     }
 
 
 
-    renderSubscribed =() => {
+    renderSubscribed = () => {
         return (
             <div style={{ width: '100%', height: '450px', position: 'relative' }}>
                 <div style={{
@@ -57,7 +56,7 @@ export class Welcome extends Component {
 
 
     render() {
-        const { user, is_superuser, currentTab } = this.state;
+        const { user, currentTab } = this.state;
         return (
             <div>
                 <header id="header">
@@ -73,43 +72,43 @@ export class Welcome extends Component {
                     </nav>
                 </header>
                 {
-                    is_superuser ? (
+                    user.is_superuser ? (
                         <div>
                             <section id="industry" className="wrapper style1 special fade">
                                 <div className="container">
                                     <br />
-                                    <a className="button"  onClick={this.clickTab} name='Subscribed'>Subscribed User</a>
+                                    <a className="button" onClick={this.clickTab} name='Subscribed'>Subscribed User</a>
                                     <a className="button" onClick={this.clickTab} name="Registered">Registered User</a>
                                     <div>
                                         {
                                             "Subscribed" === currentTab ? (
                                                 this.renderSubscribed()
-                                            ):(
-                                                this.renderRegistered()
-                                            )
+                                            ) : (
+                                                    this.renderRegistered()
+                                                )
                                         }
                                     </div>
                                 </div>
                             </section>
                         </div>
-                    ):(
-                    <div style={{ width: '100%', height: '450px', position: 'relative' }}>
-                        <div style={{
-                            position: 'absolute', top: '50%', left: '50%',
-                            marginTop: '-100px',
-                            marginLeft: '-750px',
-                            width: "100%",
-                            height: "200px",
-                        }}>
-                            <p style={{ textAlign: 'center' }}>Welcome <h2>{user.username}</h2> </p>
-                        </div>
-                    </div>
-                    )
+                    ) : (
+                            <div style={{ width: '100%', height: '450px', position: 'relative' }}>
+                                <div style={{
+                                    position: 'absolute', top: '50%', left: '50%',
+                                    marginTop: '-100px',
+                                    marginLeft: '-750px',
+                                    width: "100%",
+                                    height: "200px",
+                                }}>
+                                    <p style={{ textAlign: 'center' }}>Welcome <h2>{user.username}</h2> </p>
+                                </div>
+                            </div>
+                        )
                 }
-                
-                <div style={{position:'absolute',bottom:0, marginLeft: -350, left:'50%'}}>
-                    <footer style={{textAlign:'center', width:700}}>
-                        <ul className="icons" style={{marginBottom:20}}>
+
+                <div style={{ position: 'absolute', bottom: 0, marginLeft: -350, left: '50%' }}>
+                    <footer style={{ textAlign: 'center', width: 700 }}>
+                        <ul className="icons" style={{ marginBottom: 20 }}>
                             <li><a href="http://instagram.com/thehypeadvisor" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
                             <li><a href="mailto:team@thehypeadvisor.com" className="icon alt fa-envelope"><span className="label">Email</span></a></li>
                         </ul>
