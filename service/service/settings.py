e68d9aa193296a54d38463a6e9dda3dd82ec95cc
dang <jinling.dang@sktlab.com>
2019-10-12 00:34:43 +0800

    bug fix

--- service/service/settings.py
+++ service/service/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = 'o)=1qn!z#l=kc@orj%th%_a8ynw$p_e_owwhbc5@+4lsf7d=)2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['*']
