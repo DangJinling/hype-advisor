33b5dfb31aa102d1d5768536c3d290e0eae0b854
dang <jinling.dang@sktlab.com>
2019-10-12 13:45:03 +0800

    BUG FIX

--- client/src/actions/members.js
+++ client/src/actions/members.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 axios.defaults.withCredentials = true;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
-const server = 'http://35.230.8.228:8000';
+const server = 'http://127.0.0.1:8000';
 
 export async function addMember(member) {
     let response = axios.post(`${server}/api/subscribed/`, member);
